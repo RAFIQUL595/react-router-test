f54e0433444626be630d519d4392af06cc995031
RAFIQUL595 <rafikulislam100000@gmail.com>
2024-10-30 23:27:47 +0600

    navigation and location use done

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,13 +1,22 @@
-import { Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";
+import './Header.css'
 
 const Header = () => {
   return (
-    <div>
+    <div className="sidebar">
       <nav className="bg-orange-300 p-5 rounded-md text-pink-600">
-        <Link className="mr-5" to={"/"}>Home</Link>
-        <Link className="mr-5" to={"/users"}>Users</Link>
-        <Link className="mr-5" to={"/about"}>About Us</Link>
-        <Link className="mr-5" to={"/contact"}>Contact Us</Link>
+        <NavLink className="mr-5" to={"/"}>
+          Home
+        </NavLink>
+        <NavLink className="mr-5" to={"/users"}>
+          Users
+        </NavLink>
+        <NavLink className="mr-5" to={"/about"}>
+          About Us
+        </NavLink>
+        <NavLink className="mr-5" to={"/contact"}>
+          Contact Us
+        </NavLink>
       </nav>
     </div>
   );
--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,15 +1,44 @@
-import { Outlet } from "react-router-dom";
+import { Outlet, useLocation, useNavigation } from "react-router-dom";
 import Header from "../Header/Header";
 import Footer from "../Footer/Footer";
+import { CirclesWithBar } from "react-loader-spinner";
 
 const Home = () => {
-    return (
-        <div>
-            <Header></Header>
-            <Outlet></Outlet>
-            <Footer></Footer>
+  const navigation = useNavigation();
+  const location=useLocation();
+  console.log(location);
+
+  return (
+    <div>
+      <Header></Header>
+      {navigation.state === "loading" ? (
+        <div
+          style={{
+            display: "flex",
+            justifyContent: "center",
+            height: "100vh",
+          }}
+        >
+          <CirclesWithBar
+            height="100"
+            width="100"
+            color="#4fa94d"
+            outerCircleColor="#4fa94d"
+            innerCircleColor="#4fa94d"
+            barColor="#4fa94d"
+            ariaLabel="circles-with-bar-loading"
+            wrapperStyle={{}}
+            wrapperClass=""
+            visible={true}
+          />
         </div>
-    );
+      ) : (
+        <Outlet></Outlet>
+      )}
+
+      <Footer></Footer>
+    </div>
+  );
 };
 
 export default Home;
