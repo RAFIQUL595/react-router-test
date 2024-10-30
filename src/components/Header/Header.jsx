b1aecb8838fe5bf91362403c080d45f989e88ef8
RAFIQUL595 <rafikulislam100000@gmail.com>
2024-10-30 11:51:03 +0600

    users addd done

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,14 +1,16 @@
+import { Link } from "react-router-dom";
 
 const Header = () => {
-    return (
-        <div>
-            <nav>
-                <a href="/">Home</a>
-                <a href="/about">About Us</a>
-                <a href="/contact">Contact Us</a>
-            </nav>
-        </div>
-    );
+  return (
+    <div>
+      <nav className="bg-orange-300 p-5 rounded-md">
+        <Link className="mr-5" to={"/"}>Home</Link>
+        <Link className="mr-5" to={"/users"}>Users</Link>
+        <Link className="mr-5" to={"/about"}>About Us</Link>
+        <Link className="mr-5" to={"/contact"}>Contact Us</Link>
+      </nav>
+    </div>
+  );
 };
 
 export default Header;
---
+++ src/components/Users/Users.jsx
@@ -0,0 +1,11 @@
+
+
+const Users = () => {
+    return (
+        <div>
+            <h2>This Call Form Users</h2>
+        </div>
+    );
+};
+
+export default Users;
